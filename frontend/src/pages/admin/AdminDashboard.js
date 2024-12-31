b823b6479cfa87049ed564dbe6d944f0b15454cb
wangp126 <ericwang@gmail.com>
2024-12-30 19:19:30 -0500

    add AdminSideBar

--- frontend/src/pages/admin/AdminDashboard.js
+++ frontend/src/pages/admin/AdminDashboard.js
@@ -1,4 +1,16 @@
-
+import { useState } from "react";
+import {
+  CssBaseline,
+  Box,
+  Toolbar,
+  List,
+  Typography,
+  Divider,
+  IconButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 
 
